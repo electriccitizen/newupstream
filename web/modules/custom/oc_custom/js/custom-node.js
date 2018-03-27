6406d12aed62e78c6143c1cc76bf46fb9b1fb088
Adam Fuchs <adam@electriccitizen.com>
2018-03-27 09:34:25 -0500

    Theme documentation

--- web/modules/custom/oc_custom/js/custom-node.js
+++ web/modules/custom/oc_custom/js/custom-node.js
@@ -27,33 +27,6 @@ Drupal.behaviors.admin_bkimage = {
 	}
 };
 
-Drupal.behaviors.admin_bannerBack = {
-	attach: function (context, settings) {
-		$(".field--name-field-use-as-background", context).once('hasImage').each(function(){  
-
-			$('.field--name-field-use-as-background').each(function(){
-				if(($('input:checked', this).length) && (!$('#ief-entity-table-edit-field-banner-entities').length)){
-					$(this).prepend('<div class="custom-warning">Make sure you add a banner to use as the background.</div>');
-					setTimeout(function() {
-						$('.custom-warning').remove();
-					}, 6000);
-				}
-				$('input', this).change(function(){
-					$('.custom-warning').remove();
-					if((this.checked) && (!$('#ief-entity-table-edit-field-banner-entities').length)){
-						$('.field--name-field-use-as-background').prepend('<div class="custom-warning">Make sure you add a banner to use as the background.</div>');
-						setTimeout(function() {
-							$('.custom-warning').remove();
-						}, 3000);
-					}
-				});
-			});
-		
-		});
-	}
-};
-
-
 
 
 })(jQuery, Drupal);
