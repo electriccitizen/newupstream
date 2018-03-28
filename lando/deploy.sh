ab922ea5ebd2f34e9f65ea3cfa0fb3e8ecfe3f4c
Tim Broeker <tim@electriccitizen.com>
2018-03-28 23:21:02 +0000

    config splits

--- lando/deploy.sh
+++ lando/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# colors
-GREEN='\033[0;32m'
-DEFAULT_COLOR='\033[0;0m'
-
-# PARSE THE ARGZZ
+# SET COMMIT MSG
 while (( "$#" )); do
   case "$1" in
     -d|--db|--db=*)
@@ -19,30 +15,36 @@ while (( "$#" )); do
   esac
 done
 
+# SET DEVELOPMENT BRANCH (GITHUB)
+MASTER_BRANCH=master
+
+# DETECT ACTIVE FEATURE BRANCH (IF ANY)
+FEATURE_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+FEATURE_BRANCH="(unnamed branch)"     # detached HEAD
+FEATURE_BRANCH=${FEATURE_BRANCH##refs/heads/}
+
+# SET COLORS
+GREEN='\033[0;32m'
+DEFAULT_COLOR='\033[0;0m'
+
 # Make sure we are in the project root
 cd $LANDO_MOUNT/web
 
-#export config
+# Export config for deploy
 echo "Exporting config..."
 drush cex sync -y
 
-# Make sure any new config is added to git
+# Add any new config files to git
 echo "Adding new config to git..."
 git add ../config/*
 
-# Detect name of the feature branch
-BRANCH_NAME="$(git symbolic-ref HEAD 2>/dev/null)" ||
-BRANCH_NAME="(unnamed branch)"     # detached HEAD
-BRANCH_NAME=${BRANCH_NAME##refs/heads/}
-
-
 # Attempt to pull and merge master branch
-echo "Checking out and merging master into $BRANCH_NAME ..."
-
-
+echo "Checking out $MASTER_BRANCH..."
 git checkout master
 git pull origin master
-git checkout ${BRANCH_NAME##refs/heads/}
+
+echo "Merging into $FEATURE_BRANCH..."
+git checkout ${FEATURE_BRANCH##refs/heads/}
 git merge master
 
 # Attempt cim
@@ -50,22 +52,26 @@ echo "Importing config..."
 drush cim -y
 
 # Updatedb
-echo "Updating db..."
+echo "Updating database..."
 drush updb -y
 
 # Clear cache
 echo "Clearing cache..."
 drush cr
 
+# Deploy feature branch to Github
 while true; do
     #read -p "Proceed with deploy (y/n)?" yn
-    read -p "$(echo -e $GREEN"Proceed with the deploy [y/n]? "$DEFAULT)" yn
+    read -p $'\033[0;32mProceed with deploy [y/n]?\033[0;0m ' yn
+    #read -p "$(echo -e $GREEN"Proceed with the deploy [y/n]? "$DEFAULT)" yn
     case $yn in
         [Yy]* ) git commit -am "$MSG"; git push origin $BRANCH_NAME; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+
 
 
 
--- lando/push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "YOU ARE NOT ALLOWED TO USE THIS COMMAND";
-exit;
