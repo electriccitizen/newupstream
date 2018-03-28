8cb27631a53c5e5cdabc66b3b15394632eb2f7b9
Tim Broeker <tim@electriccitizen.com>
2018-03-28 17:52:26 +0000

    update lando

--- lando/deploy.sh
+++ lando/deploy.sh
@@ -31,35 +31,45 @@ echo "Adding new config to git..."
 git add ../config/*
 
 # Detect name of the feature branch
-FEATURE_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
-FEATURE_BRANCH="(unnamed branch)"     # detached HEAD
-FEATURE_BRANCH=${FEATURE_BRANCHs##refs/heads/}
+BRANCH_NAME="$(git symbolic-ref HEAD 2>/dev/null)" ||
+BRANCH_NAME="(unnamed branch)"     # detached HEAD
+BRANCH_NAME=${BRANCH_NAME##refs/heads/}
+
 
 # Attempt to pull and merge master branch
-echo "Checking out and merging master into $FEATURE_BRANCH ..."
+echo "Checking out and merging master into $BRANCH_NAME ..."
+
+
 git checkout master
 git pull origin master
-git checkout ${branch_name##refs/heads/}
+git checkout ${BRANCH_NAME##refs/heads/}
 git merge master
 
 # Attempt cim
 echo "Importing config..."
-#drush cim -y
+drush cim -y
 
 # Updatedb
 echo "Updating db..."
-#drush updb -y
+drush updb -y
 
 # Clear cache
 echo "Clearing cache..."
-#drush cr
+drush cr
 
 while true; do
     #read -p "Proceed with deploy (y/n)?" yn
-    read -p "$(echo -e $GREEN"Proceed with the deploy (y/n)? "$DEFAULT)" yn
+    read -p "$(echo -e $GREEN"Proceed with the deploy [y/n]? "$DEFAULT)" yn
     case $yn in
         [Yy]* ) git commit -am "$MSG"; git push origin $FEATURE_BRANCH break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+
+
+
+
+
+
---
+++ lando/push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "YOU ARE NOT ALLOWED TO USE THIS COMMAND";
+exit;
