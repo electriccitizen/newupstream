399b076372104e1c4dc0e1d2caec6e13014fdce4
Tim Broeker <tim@electriccitizen.com>
2018-05-01 23:22:49 -0500

    docksal updates

---
+++ .docksal/commands/common/sync_db.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Import a database from a Pantheon environment.
+##
+## Usage: this function is included in various custom fin commands via:
+## source "${PROJECT_ROOT}/.docksal/commands/common/dbsync.sh"
+
+sync_db ()
+{
+  echo "fooman" $1;
+}
