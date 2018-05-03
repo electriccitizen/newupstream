63be006f5746b10faa30b797711ca0c0c6d42022
Tim Broeker <tim@electriccitizen.com>
2018-05-03 16:36:50 -0500

    docksal

--- .docksal/commands/common/sync_config.sh
+++ .docksal/commands/common/sync_config.sh
@@ -11,7 +11,12 @@ green='\033[0;32m'
 green_bg='\033[42m'
 yellow='\033[1;33m'
 NC='\033[0m'
+bg='\[\033[0;35m\]'
 
+echo-red () { echo -e "${red}$1${NC}"; }
+echo-green () { echo -e "${green}$1${NC}"; }
+echo-green-bg () { echo -e "${green_bg}$1${NC}"; }
+echo-yellow () { echo -e "${yellow}$1${NC}"; }
 
 sync_config ()
 {
@@ -23,7 +28,7 @@ echo -e  "${green_bg} Step 3 ${NC}${green} Pulling $GIT_BRANCH branch from Githu
 git pull origin $GIT_BRANCH
 
 # Run composer install
-echo -e "${green_bg} Step 3 ${NC}${green} Running composer install...${NC}"
+echo -e "${green_bg} Step 4 ${NC}${green} Running composer install...${NC}"
 composer install
 
 # No idea why this is needed but i abide
@@ -34,19 +39,19 @@ if is_windows; then
 fi
 
 # Configure things for local development environment.
-echo -e "${green_bg} Step 4 ${NC}${green} Importing config...${NC}"
+echo -e "${green_bg} Step 5 ${NC}${green} Importing config...${NC}"
 fin drush cim -y
 
-echo -e "${green_bg} Step 5 ${NC}${green} Running db updates...${NC}"
+echo -e "${green_bg} Step 6 ${NC}${green} Running db updates...${NC}"
 fin drush updb -y
 
-echo -e "${green_bg} Step 6 ${NC}${green} Running entity updtes...${NC}"
+echo -e "${green_bg} Step 7 ${NC}${green} Running entity updtes...${NC}"
 fin drush entup -y
 
-echo -e "${green_bg} Step 7 ${NC}${green} Clearing caches...${NC}"
+echo -e "${green_bg} Step 8 ${NC}${green} Clearing caches...${NC}"
 fin drush cr all
 
-echo -en "${green_bg} DONE! ${NC} "
+echo -en "${green_bg} SYNC COMPLETE! ${NC} "
 echo -e "Open ${yellow}http://${VIRTUAL_HOST}${NC} in your browser to verify the setup."
 
 }
--- .docksal/commands/common/sync_db.sh
+++ .docksal/commands/common/sync_db.sh
@@ -13,7 +13,6 @@ green='\033[0;32m'
 green_bg='\033[42m'
 yellow='\033[1;33m'
 NC='\033[0m'
-
 bg='\[\033[0;35m\]'
 
 echo-red () { echo -e "${red}$1${NC}"; }
