c330c70262d4c0771f1409ebf3232d26182655fa
Adam Fuchs <adam@electriccitizen.com>
2018-05-23 11:13:18 -0500

    Theme file cleanup, take select 2 off mana ge form displays and manage displays

--- web/modules/custom/oc_custom/js/custom-global.js
+++ web/modules/custom/oc_custom/js/custom-global.js
@@ -5,7 +5,7 @@ Drupal.behaviors.mobileSelect = {
 	attach: function (context, settings) {
       $("select", context).once('selects').each(function(){  
 	      //if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))) {
-	      	$( 'form:not(.entity-embed-dialog) select,.sg-pattern-example select' ).select2({
+	      	$( 'form:not(.entity-embed-dialog):not(.entity-form-display-form):not(.entity-view-display-form) select,.sg-pattern-example select' ).select2({
 	      		placeholder: "Select an option"
 	      	});
 	      //}
