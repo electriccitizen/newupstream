1fe6e13cc801a0d078586d75d9b5f10260d14331
Adam Fuchs <adam@electriccitizen.com>
2018-05-23 12:12:36 -0500

    Clean up call to action, libraries, remove simple block

--- web/themes/oc_patterns/components/_patterns/03-organisms/02-widgets/lightbox/lightbox.js
+++ web/themes/oc_patterns/components/_patterns/03-organisms/02-widgets/lightbox/lightbox.js
@@ -14,28 +14,5 @@ Drupal.behaviors.lightbox = {
 	}
 }
 
-/*
-//Fade-in class for lightboxes when in viewframe.
-Drupal.behaviors.galleryFadeIn = {
-	attach: function (context, settings) {
-		$(".paragraph--type--lightbox-gallery", context).once('randomBoxes').each(function(){
-			//animate when the page scrolls down far enough
-			$(window).on("load", function(){
-				randomEntrance();
-				$(document).scroll(function() {
-					randomEntrance();
-				});
-				function randomEntrance() {
-					$('.paragraph--type--lightbox-gallery').each(function(){
-						if($(document).scrollTop() + window.innerHeight > $(this).offset().top + 50){
-							$('.media-image.view-mode-large-thumb', this).addClass('randomEntrance');
-						}
-					});
-				}
-			});
-		});
-	}
-}
-*/
 
 })(jQuery, Drupal);
