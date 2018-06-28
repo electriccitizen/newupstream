f7e7a48c74a0346f52c421ef37f904f9a0efce6a
Adam Fuchs <adam@electriccitizen.com>
2018-06-28 10:43:31 -0500

    Fix accordions based on MPLS accessibility

--- web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/accordion/accordion.js
+++ web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/accordion/accordion.js
@@ -1,21 +1,32 @@
 (function($, Drupal) {
 
 Drupal.behaviors.accordion = {
-	attach: function (context, settings) {
-		$(".accordion-item", context).once('accordion').each(function(){  
-  	 		//open and shut accordions on click
-        $('.accordion-header', this).click(function(e){
+    attach: function (context, settings) {
+      $(".accordion-item", context).once('accordion').each(function(){  
+        //open and shut accordions on click
+        $('.accordion-header a', this).click(function(e){
           e.preventDefault();
-          if($(this).parent('.accordion-item.accord-active').length){
-            $('.accord-active').removeClass('accord-active').attr('aria-expanded', "false");
-            $(this).next().slideUp(500);
+          var activeHeader = $(this).parent('.accordion-header');
+          if(activeHeader.closest('.accordion-item.accord-active').length){
+            $('.accord-active').removeClass('accord-active');
+            activeHeader.next().slideUp(500).attr('aria-hidden', 'true').end().find('a').attr('aria-expanded', "false");
           }else{
-            $('.accord-active').find('.field-oc-long-text').slideUp(500).end().removeClass('accord-active').attr('aria-expanded', 'false');
-            $(this).parent('.accordion-item').addClass('accord-active').attr('aria-expanded', "true").end().next().slideDown(500);
+            $('.accord-active').find('.field-oc-long-text').slideUp(500).attr('aria-hidden', 'true').end().removeClass('accord-active').find('.accordion-header a').attr('aria-expanded', 'false');
+            activeHeader.parent('.accordion-item').addClass('accord-active').end().next().slideDown(500).attr('aria-hidden', 'false').end().find('a').attr('aria-expanded', "true");
+            //detect if accordion top is offscreen and scroll to it if it is
+            setTimeout(function() {
+              var windowTop = $(window).scrollTop();
+              var currentAccordion = $('.accord-active').offset().top;
+              if(windowTop > currentAccordion){
+                $('html, body').animate({
+                        scrollTop: $('.accord-active').offset().top - 100
+                    });
+              }
+            }, 510);
           }
         }); 
-		});
-	}
+      });
+    }
 }
 
 })(jQuery, Drupal);
