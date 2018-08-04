a707e4805834af3850102625769c819c24b71aeb
Adam Fuchs <adam@electriccitizen.com>
2018-08-04 16:14:12 -0500

    Redo menu block to work with layout builder edit config

--- web/themes/oc_patterns/components/_patterns/02-molecules/menus/02-page-menu/page-menu.js
+++ web/themes/oc_patterns/components/_patterns/02-molecules/menus/02-page-menu/page-menu.js
@@ -2,15 +2,26 @@
 Drupal.behaviors.pageMenu = {
 	attach: function (context, settings) {
 		$(".block--system-menu.page-menu", context).once('page-menu').each(function(){  
+			//mobile toggle
+			$('.page-menu-toggle').click(function(e){
+				e.preventDefault();
+				if($('.page-menu-toggle.open').length){
+					$('#page-menu-wrapper').slideUp(300).attr('aria-hidden', 'true');
+					$('.page-menu-toggle').find('i').removeClass('fa-times-circle').addClass('fa-bars').end().attr('aria-expanded', 'false').removeClass('open');
+				}else{
+					$('#page-menu-wrapper').slideDown(300).attr('aria-hidden', 'false');
+					$('.page-menu-toggle').find('i').removeClass('fa-bars').addClass('fa-times-circle').end().attr('aria-expanded', 'true').addClass('open');
+				}
+			});
+					
 			//need doc ready because active-class script fires after theme scripts
 			$(document).ready(function(){
-				$('.block--system-menu.page-menu > ul li').each(function(){
-					
+				$('#page-menu-wrapper ul li').each(function(){			
 					//find active links and set the active trail
-					$('.is-active', this).parentsUntil('.page-menu').addClass('active-trail expanded');
+					$('.is-active', this).parentsUntil('#page-menu-wrapper > ul').addClass('active-trail expanded');
 
 					//find nested lists and set their parents and expanders
-					if(($('> ul', this).length) && (!$('.expander:first', this).length) ){
+					if(($('#page-menu-wrapper > ul', this).length) && (!$('.expander:first', this).length) ){
 					  $(this).addClass('parent').prepend('<button class="expander" aria-expanded="false"></button>');
 					}
 
@@ -20,7 +31,7 @@ Drupal.behaviors.pageMenu = {
 				});
 
 				//set button roles, tab indexes and keypresses on sidebar links
-				$('.block--system-menu.page-menu a').each(function(i){
+				$('#page-menu-wrapper a').each(function(i){
 					//find heirarchies in the menu and add a class to each link
 					$(this).addClass('level-' + $(this).parents('.page-menu ul').length);
 					$(this).attr('tabindex', '0');
