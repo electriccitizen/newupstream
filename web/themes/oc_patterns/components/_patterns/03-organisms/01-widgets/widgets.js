4c26acc0c16c3e1c3c54296a0ed12297de9488db
Adam Fuchs <adam@electriccitizen.com>
2018-08-07 19:05:07 -0500

    Fix section nav padding, add html 5 nav tag to anchor nav and set proper aria roles

--- web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/widgets.js
+++ web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/widgets.js
@@ -1,38 +1,58 @@
 (function ($) {
 
-	Drupal.behaviors.widgetAnchors = {
-	  attach: function (context, settings) {
-	    $(".widget-anchors", context).once('smoothscroll').each(function(){  
-	    	//open in this section
-	    	$('h2',this).click(function(){
-	    		$('.anchors').toggleClass('expanded');
-	    		if($('.anchors').is('.expanded')){
-	    			$('.toc-toggle').html('&times;');
-	    			$('.anchors').slideDown(200);
-	    			$('.anchors').attr('aria-expanded', 'true');
-	    		}else{
-	    			$('.toc-toggle').html('&#9776');
-	    			$('.anchors').slideUp(200);
-	    			$('.anchors').attr('aria-expanded', 'false');
-	    		}
-	    	});
+Drupal.behaviors.widgetAnchors = {
+	attach: function (context, settings) {
+		$(".widget-anchors", context).once('smoothscroll').each(function(){  
+    	//open in this section
+    	$('.toc-toggle').click(function(e){
+    		e.preventDefault();
+	      if($(window).outerWidth() < 760){
+	        if($(this).is('.active-nav')){
+	          $(this).attr('aria-expanded', 'false').removeClass('active-nav').find('i').removeClass('fa-times-circle').addClass('fa-bars').closest('#toc-title').next('#toc-anchor-wrapper').attr('aria-hidden', 'true').slideUp(500);
+	        }else{
+	          $(this).attr('aria-expanded', 'true').addClass('active-nav').find('i').removeClass('fa-bars').addClass('fa-times-circle').closest('#toc-title').next('#toc-anchor-wrapper').attr('aria-hidden', 'false').slideDown(500);
+	        }
+	      }
+    	});
 
-	    	//scroll to anchors
-	    	$('a', this).click(function(e){
-	    		e.preventDefault();
-	    		var anchor = $(this).attr('href');
-	    		$('.anchors').slideUp(10).removeClass('expanded');
-	    		$('.anchors').attr('aria-expanded', 'false');
-	    		$('.toc-toggle').html('&#9776');
-	    		setTimeout(function(){
-	    			$('html, body').animate({
-               	 scrollTop: $(anchor).offset().top - 100
-            	});
-	    		}, 20);
-	    	});
+    	$(window).on('resize',  _.debounce( mobileAnchorNav, 10 )).trigger('resize');
 
-	   });
-	  }
-	};
+    	//scroll to anchors
+    	$('a:not(.toc-toggle)', this).click(function(e){
+    		e.preventDefault();
+    		//close the anchor nav and reset arias
+    		$('.toc-toggle.active-nav').attr('aria-expanded', 'false').removeClass('active-nav').find('i').removeClass('fa-times-circle').addClass('fa-bars').closest('#toc-title').next('#toc-anchor-wrapper').attr('aria-hidden', 'true').slideUp(10);
+    		//scroll to the anchor
+    		var anchor = $(this).attr('href');
+    		setTimeout(function(){
+    			$('html, body').animate({
+            	 scrollTop: $(anchor).offset().top - 100
+         	});
+    		}, 20);
+    	});
+   });
+  }
+};//end widget anchors
+
+function mobileAnchorNav() {
+  var wwidth = $(window).outerWidth();
+  if (wwidth < 760) {
+    //add aria roles to anchors title and wrapper if not already set by click above 
+    if(!$('.toc-toggle').attr('aria-controls')){
+      $('.toc-toggle').attr({
+        'aria-controls': 'toc-anchor-wrapper', 
+        'aria-expanded': 'false', 
+        'role': 'button'
+      });
+      $('#toc-anchor-wrapper').attr('aria-hidden', 'true');
+    } 
+  }else{
+    //strip all aria roles & prevent click
+    $('.toc-toggle').removeAttr('aria-controls aria-expanded role');
+    $('#toc-anchor-wrapper').removeAttr('aria-hidden');
+  }
+};
+
+
 
 })(jQuery);
