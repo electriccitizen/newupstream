2cace484b5249bd1f908fbdd8615e161b351ecf5
Christine Coleman <christine@electriccitizen.com>
2018-12-04 12:06:36 -0600

    Working on UP24

---
+++ web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/gallery/gallery.js
@@ -0,0 +1,39 @@
+(function($, Drupal) {
+
+//Swipebox script for lightbox images.
+Drupal.behaviors.lightbox = {
+	attach: function (context, settings) {
+		$(".paragraph--type--gallery", context).once('lightboxes').each(function(){
+			$('.featherlight-gal', this).lightboxGallery({
+				previousIcon: '<',
+				nextIcon: '>',
+				galleryFadeIn: 300,
+				openSpeed: 300
+			});
+		});
+	}
+}
+
+//Fade-in class for lightboxes when in viewframe.
+Drupal.behaviors.galleryFadeIn = {
+	attach: function (context, settings) {
+		$(".paragraph--type--gallery", context).once('randomBoxes').each(function(){
+			//animate when the page scrolls down far enough
+			$(window).on("load", function(){
+				randomEntrance();
+				$(document).scroll(function() {
+					randomEntrance();
+				});
+				function randomEntrance() {
+					$('.paragraph--type--gallery').each(function(){
+						if($(document).scrollTop() + window.innerHeight > $(this).offset().top + 50){
+							$('.media-image.view-mode-large-thumb', this).addClass('randomEntrance');
+						}
+					});
+				}
+			});
+		});
+	}
+}
+
+})(jQuery, Drupal);
--- web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/lightbox/lightbox.js
+++ web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/lightbox/lightbox.js
@@ -1,18 +1,19 @@
-(function($, Drupal) {
+/*(function($, Drupal) {
 
 //Swipebox script for lightbox images.
 Drupal.behaviors.lightbox = {
 	attach: function (context, settings) {
-		$(".paragraph--type--lightbox-gallery", context).once('lightboxes').each(function(){  
-			$('.featherlight-gal', this).featherlightGallery({
+		$(".paragraph--type--lightbox-gallery", context).once('lightboxes').each(function(){
+	//		$('.featherlight-gal', this).featherlightGallery({
 				previousIcon: '<',
 				nextIcon: '>',
 				galleryFadeIn: 300,
 				openSpeed: 300
-			});
+		//	});
 		});
 	}
 }
 
 
 })(jQuery, Drupal);
+*/
