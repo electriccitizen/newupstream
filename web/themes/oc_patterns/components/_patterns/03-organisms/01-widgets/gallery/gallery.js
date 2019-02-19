5a4fd0e485e6a1ed78b634c3c7baebd6f5ae4d39
Christine Coleman <christine@electriccitizen.com>
2019-02-19 14:53:37 -0600

    Worked on combining the slideshow and lightbox gallery widgets

--- web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/gallery/gallery.js
+++ web/themes/oc_patterns/components/_patterns/03-organisms/01-widgets/gallery/gallery.js
@@ -1,31 +1,27 @@
 
 (function($, Drupal) {
 
-//Swipebox script for lightbox images.
-Drupal.behaviors.gallery = {
-	attach: function (context, settings) {
-
-		$(".Lightbox", context).once('lightboxes').each(function(){
-			$('.featherlight-gal', this).featherlightGallery({
-				previousIcon: '<',
-				nextIcon: '>',
-				galleryFadeIn: 300,
-				openSpeed: 300
+	//Swipebox script for lightbox images.
+	Drupal.behaviors.gallery = {
+		attach: function (context, settings) {
+			//Featherlight
+			$(".gallery-type.Lightbox", context).once('lightboxes').each(function(){
+				$('.featherlight-gal', this).featherlightGallery({
+					previousIcon: '<',
+					nextIcon: '>',
+					galleryFadeIn: 300,
+					openSpeed: 300
+				});
 			});
-		});
-
-
-
-//Swipebox script for lightbox images.
-$(".paragraph--type--slideshow", context).once('slider').each(function(){
-  $('.field-oc-image-multi', this).slick({
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 5000
-  });
-});
-
-    }
-}
+			//Slider
+			$(".gallery-type.Slider", context).once('slider').each(function(){
+				$('.field-oc-image-multi', this).slick({
+					adaptiveHeight: true,
+					autoplay: true,
+					autoplaySpeed: 5000
+				});
+			});
+		}
+	}
 
 })(jQuery, Drupal);
