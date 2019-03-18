d774ac7f9c0c4ce31ff27e85d89ab6316b777df8
broeker <tim@electriccitizen.com>
2019-03-18 14:56:30 -0500

    UP-00: update pantheon alias.

--- drush/newupstream.aliases.drushrc.php
+++ drush/newupstream.aliases.drushrc.php
@@ -1,14 +1,14 @@
 <?php
-$aliases['newupstream.dev'] = array(
-  'uri' => 'dev-newupstream.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:8f51e4928f7a4e3fbc32bc564d772aad@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:14558/pantheon',
-  'db-allows-remote' => TRUE,
-  'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
-  'remote-user' => 'dev.8be50dbc-4463-43b5-bd18-d83046623820',
-  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
-  'path-aliases' => array(
-    '%files' => 'files',
-    '%drush-script' => 'drush',
-  ),
-);
 
+$aliases['newupstream.dev'] = array(
+    'uri' => 'dev-newupstream.pantheonsite.io',
+    'db-url' => 'mysql://pantheon:9d228146cdb9421ab93d99a8d27b18d7@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:22277/pantheon',
+    'db-allows-remote' => TRUE,
+    'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
+    'remote-user' => 'dev.8be50dbc-4463-43b5-bd18-d83046623820',
+    'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+    'path-aliases' => array(
+        '%files' => 'files',
+        '%drush-script' => 'drush',
+    ),
+);
