bace74868bb5596d02e2b2c0f8d1cfcc61e0ec28
Adam Fuchs <adam@electriccitizen.com>
2019-08-16 10:33:15 -0500

    Modal tweaks, remove media js

--- web/themes/citizen_patterns/dist/02-molecules/media/_media.js
+++
@@ -1,2 +0,0 @@
-"use strict";(function(a,b){/* distinguish video from other oc media */b.behaviors.ocMediaEmbedded={attach:function attach(b){a(".citizen-media .field-media-video-embed-field",b).once("isVideo").each(function(){a(this).closest(".citizen-media").addClass("has-video")})}}})(jQuery,Drupal);
-//# sourceMappingURL=_media.js.map
