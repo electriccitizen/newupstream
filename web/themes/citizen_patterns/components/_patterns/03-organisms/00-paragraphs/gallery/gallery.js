2d3cd59ee458c0248bf3b59acb059dd7999ff992
Adam Fuchs <adam@electriccitizen.com>
2019-08-16 17:03:10 -0500

    Paragraph preview theming

--- web/themes/citizen_patterns/components/_patterns/03-organisms/00-paragraphs/gallery/gallery.js
+++ web/themes/citizen_patterns/components/_patterns/03-organisms/00-paragraphs/gallery/gallery.js
@@ -6,19 +6,23 @@
 		attach: function (context, settings) {
 			//Featherlight
 			$(".gallery-type.Lightbox", context).once('lightboxes').each(function(){
-				$('.featherlight-gal', this).featherlightGallery({
-					previousIcon: '<',
-					nextIcon: '>',
-					galleryFadeIn: 300,
-					openSpeed: 300
+				$(document).ready(function(){
+					$('.featherlight-gal', this).featherlightGallery({
+						previousIcon: '<',
+						nextIcon: '>',
+						galleryFadeIn: 300,
+						openSpeed: 300
+					});
 				});
 			});
 			//Slider
 			$(".gallery-type.Slider", context).once('slider').each(function(){
-				$('.field-image-multi', this).slick({
-					adaptiveHeight: true,
-					autoplay: true,
-					autoplaySpeed: 5000
+				$(document).ready(function(){
+					$('.field-image-multi', this).slick({
+						adaptiveHeight: true,
+						autoplay: true,
+						autoplaySpeed: 5000
+					});
 				});
 			});
 		}
