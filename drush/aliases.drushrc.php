282a48687a210a841bf1860e7cb65f430926e9de
Adam Fuchs <adam@electriccitizen.com>
2019-09-20 16:53:11 -0500

    alias fuckery

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -13,13 +13,13 @@ $aliases['newupstream.local'] = array(
 
 $aliases['newupstream.dev'] = array(
     'uri' => 'dev-newupstream.pantheonsite.io',
-    'db-url' => 'mysql://pantheon:9d228146cdb9421ab93d99a8d27b18d7@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:22277/pantheon',
+    'db-url' => 'mysql://pantheon:880a9ab927944e21b023a428f715d98d@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:10796/pantheon',
     'db-allows-remote' => TRUE,
     'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
     'remote-user' => 'dev.8be50dbc-4463-43b5-bd18-d83046623820',
     'ssh-options' => '-p 2222 -o "AddressFamily inet"',
     'path-aliases' => array(
-        '%files' => 'files',
-        '%drush-script' => 'drush',
-    ),
-);
+      '%files' => 'files',
+      '%drush-script' => 'drush',
+     ),
+  );
