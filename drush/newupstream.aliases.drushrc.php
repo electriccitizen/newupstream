2a1db6978d40def24f276feb77390a010647d2ac
Adam Fuchs <adam@electriccitizen.com>
2019-09-20 16:44:31 -0500

    test

--- drush/newupstream.aliases.drushrc.php
+++ drush/newupstream.aliases.drushrc.php
@@ -2,7 +2,7 @@
 
 $aliases['newupstream.dev'] = array(
     'uri' => 'dev-newupstream.pantheonsite.io',
-    'db-url' => 'mysql://pantheon:9d228146cdb9421ab93d99a8d27b18d7@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:22277/pantheon',
+    'db-url' => 'mysql://pantheon:9d228146cdb9421ab93d99a8d27b18d7@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
     'db-allows-remote' => TRUE,
     'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
     'remote-user' => 'dev.8be50dbc-4463-43b5-bd18-d83046623820',
