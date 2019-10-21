7f539b4cc5289e3501c59e81cff389fb352baef8
Adam Fuchs <adam@electriccitizen.com>
2019-10-21 15:40:16 -0500

    Remove gzip taska nd mins

--- web/themes/citizen_patterns/gulpfile.js
+++ web/themes/citizen_patterns/gulpfile.js
@@ -6,9 +6,6 @@
   // General
   var gulp = require('gulp-help')(require('gulp'));
   var localConfig = {};
-  var gzip = require('gulp-gzip');
-  var uglify = require('gulp-uglify');
-  var rename = require('gulp-rename');
 
   try {
     localConfig = require('./local.gulp-config');
@@ -17,17 +14,5 @@
     // Do nothing.
   }
   require('emulsify-gulp')(gulp, localConfig);
-
-  gulp.task('zipScript', function() {
-    gulp.src('./components/_patterns/**/*.js')
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gzip())
-    .pipe(gulp.dest('./dist'));
-  });
-
-  gulp.task('watchZip', function() {
-    gulp.watch('./components/_patterns/**/*.js', ['zipScript']);
-  });
 
 })();
