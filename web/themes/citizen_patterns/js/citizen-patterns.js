5beba0b463a63099c113fc4b6e098611b4fc8a78
Adam Fuchs <adam@electriccitizen.com>
2019-11-13 13:30:10 -0600

    Updates

--- web/themes/citizen_patterns/js/citizen-patterns.js
+++
@@ -1,9 +0,0 @@
-/* Example Behavior */
-// Drupal.behaviors.example = {
-//   attach: function (context, settings) {
-//     $(".something", context).once('exampleSomething').each(function(){  
-//       scripts go here
-//     });
-//   }
-// };
-
