9bc5c150c41f56a71134729b66946cd5c297faf0
Adam Fuchs <adam@electriccitizen.com>
2019-11-15 15:33:57 -0600

    PL updates

--- web/themes/citizen_patterns/build/dist/02-molecules/blocks/search/search-block.js
+++
@@ -1,4 +0,0 @@
-"use strict";(function(a,b){//search toggle
-b.behaviors.searchToggle={attach:function attach(){a(".block-search-form-block").once("tSearch").each(function(){a(".t-search",this).click(function(b){//close main menu if open when search is clicked
-b.preventDefault(),a("#search-form-wrapper").slideDown(400).attr("aria-hidden","false"),a(".t-search-close").delay(300).fadeIn(300),a("#superfish-main-accordion.sf-expanded").slideUp(200),setTimeout(function(){a("#superfish-main-toggle,#superfish-main-accordion").removeClass("sf-expanded")},200)}),a(".close-search",this).click(function(b){b.preventDefault(),a(".t-search-close").fadeOut(300),a("#search-form-wrapper").delay(200).slideUp(400).attr("aria-hidden","true")})})}}})(jQuery,Drupal);
-//# sourceMappingURL=search-block.js.map
